9efb9d69cb99303476d87cf10ae98e377f16cecc
ShimanGleb <alien-g.shiman@yandex.ru>
2015-03-12 17:13:07 +0300

    Update ATM.c

--- ATM.c
+++ ATM.c
@@ -7,8 +7,36 @@ namespace FOOPLab1
 {
     class ATM
     {
-        SortedList<int, int> notes = new SortedList<int, int>();
+        SortedList<int, int> notes = new SortedList<int, int>();        
         public long totalCash = 0;
+        public long GiveCash(long amount)
+        {
+            if (totalCash >= amount)
+            {
+                long tryCash = 0;
+                SortedList<int, int> givenNotes = new SortedList<int, int>();
+                while (tryCash != amount)
+                {
+                    for (int i = 0; i < notes.Keys.Count; i++)
+                    {
+                        tryCash += notes.Keys[i];
+                        if (tryCash > amount)
+                        {
+                            tryCash -= notes.Keys[i];
+                        }
+                        else
+                        {
+                            try { givenNotes.Add(notes.Keys[i], 0); }
+                            catch { givenNotes[notes.Keys[i]]++; }
+                        }
+                    }
+                }
+
+                totalCash -= amount;
+            }
+            else return -1;
+            return amount;            
+        }
         public ATM()
         {
             const string fileName = "cash.txt";
