5063101229cd1d78635f33cd5b2c566f0bb22d39
ShimanGleb <alien-g.shiman@yandex.ru>
2015-03-17 20:45:24 +0300

    Update ATM.c

--- ATM.c
+++ ATM.c
@@ -1,4 +1,4 @@
-using System;
+uusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,42 +7,35 @@ namespace FOOPLab1
 {
     class ATM
     {
-        SortedList<int, int> notes = new SortedList<int, int>();        
+        SortedList<int, int> notes = new SortedList<int, int>();
+        SortedList<int, int> givenNotes = new SortedList<int, int>();
         public long totalCash = 0;
+        public long Search(ref long tryCash,long requiredAmount)
+        {
+            for (int i = 0; i < notes.Keys.Count; i++)
+            {
+                tryCash += notes.Keys[i];
+                if (tryCash > requiredAmount) tryCash -= notes.Keys[i];
+                if (tryCash == requiredAmount)
+                {
+                    try { givenNotes[notes.Keys[i]]++; }
+                    catch { givenNotes.Add(notes.Keys[i], 0); }
+                    return tryCash;
+                }
+                else
+                {
+                    Search(ref tryCash, requiredAmount);
+                }
+            }
+            return 0;
+        }
         public long GiveCash(long amount)
         {
             if (totalCash >= amount)
             {
                 long tryCash = 0;
-                SortedList<int, int> givenNotes = new SortedList<int, int>();
-                int skips = 0;
-                while (tryCash != amount)
-                {
-                    bool added = false;
-                    for (int i = 0; i < notes.Keys.Count; i++)
-                    {                        
-                        tryCash += notes.Keys[i];
-                        if (tryCash > amount || notes[notes.Keys[i]]<0)
-                        {
-                            tryCash -= notes.Keys[i];                            
-                        }
-                        else
-                        {
-                            added=true;
-                            skips = 0;
-                            try { givenNotes.Add(notes.Keys[i], 0); }
-                            catch { givenNotes[notes.Keys[i]]++; }
-                        }
-                    }
-                    if (added == false)
-                    {
-                        skips++;
-                        tryCash -= givenNotes[notes.Keys[notes.Keys.Count/2]];
-                        if (givenNotes[notes.Keys[notes.Keys.Count / 2]] > 0) givenNotes[notes.Keys[notes.Keys.Count / 2]]--;
-                        else givenNotes.Remove(notes.Keys.Count / 2);
-                        if (skips == notes.Keys.Count) return 0;
-                    }
-                }
+                givenNotes = new SortedList<int, int>();
+                Search(ref tryCash, amount);
                 for (int i = 0; i < givenNotes.Keys.Count; i++)
                 {
                     notes[notes.Keys[i]] -= givenNotes[notes.Keys[i]];
