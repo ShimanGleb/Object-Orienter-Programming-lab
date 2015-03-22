7baf5e7f6aff12098e1b0440080cdfa89479883b
ShimanGleb <alien-g.shiman@yandex.ru>
2015-03-22 13:41:29 +0300

    Update ATM.c

--- ATM.c
+++ ATM.c
@@ -1,4 +1,4 @@
-uusing System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,58 +7,76 @@ namespace FOOPLab1
 {
     class ATM
     {
-        SortedList<int, int> notes = new SortedList<int, int>();
+        public SortedList<int, int> notes = new SortedList<int, int>();
         SortedList<int, int> givenNotes = new SortedList<int, int>();
-        public long totalCash = 0;
+        public long totalCash = 0;        
         public long Search(ref long tryCash,long requiredAmount)
         {
-            for (int i = 0; i < notes.Keys.Count; i++)
+            for (int i = notes.Keys.Count-1; i >=0 ; i--)
             {
                 tryCash += notes.Keys[i];
-                if (tryCash > requiredAmount) tryCash -= notes.Keys[i];
-                if (tryCash == requiredAmount)
+                if (tryCash > requiredAmount)
                 {
-                    try { givenNotes[notes.Keys[i]]++; }
-                    catch { givenNotes.Add(notes.Keys[i], 0); }
-                    return tryCash;
+                    tryCash -= notes.Keys[i];
                 }
                 else
                 {
-                    Search(ref tryCash, requiredAmount);
+                    try { givenNotes[notes.Keys[i]]++; }
+                    catch { givenNotes.Add(notes.Keys[i], 0); }
+                    if (tryCash < requiredAmount)
+                    {
+                        
+                        Search(ref tryCash, requiredAmount);
+                    }
+                    if (tryCash == requiredAmount)
+                    {
+                        
+                        return tryCash;
+                    }
+                }
+                if (i == 0)
+                {
+                    try
+                    {
+                        if (givenNotes[notes.Keys[i]] > 0)
+                        {
+
+                            givenNotes[notes.Keys[i]]--;
+
+                        }
+                        else givenNotes.Remove(notes.Keys[i]);
+                    }
+                    catch { }
                 }
             }
             return 0;
         }
-        public long GiveCash(long amount)
+        public MoneyOuter GiveCash(long amount)
         {
+            MoneyOuter message = new MoneyOuter();
             if (totalCash >= amount)
             {
                 long tryCash = 0;
                 givenNotes = new SortedList<int, int>();
-                Search(ref tryCash, amount);
-                for (int i = 0; i < givenNotes.Keys.Count; i++)
+
+                if (Search(ref tryCash, amount) != 0)
                 {
-                    notes[notes.Keys[i]] -= givenNotes[notes.Keys[i]];
+                    for (int i = 0; i < givenNotes.Keys.Count; i++)
+                    {
+                        notes[givenNotes.Keys[i]] -= givenNotes[givenNotes.Keys[i]] + 1;
+                    }
+                    totalCash -= amount;
+                    message.amount = amount;
+                    return message;
                 }
-                totalCash -= amount;
-            }
-            else return 0;
-            return amount;            
-        }
-        public ATM()
-        {
-            const string fileName = "cash.txt";
-            try
-            {
-                string[] strings = System.IO.File.ReadAllLines(fileName);
-                foreach (string note in strings)
+                else
                 {
-                    string[] str = note.Split(' ');
-                    totalCash += Convert.ToInt32(str[0]) * Convert.ToInt32(str[1]);
-                    notes.Add(Convert.ToInt32(str[0]), Convert.ToInt32(str[1]));
+                    message.errorType = "ATM doesn`t have suitable notes.";
+                    return message;
                 }
             }
-            catch { }
-        }
-    }
+            message.errorType = "ATM doesn`t have enough money.";
+            return message; 
+        }        
+    }    
 }
