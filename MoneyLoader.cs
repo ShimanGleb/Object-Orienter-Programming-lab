4715a46199895dca2858ea61c7bbc5aab7dae781
ShimanGleb <alien-g.shiman@yandex.ru>
2015-03-22 13:42:21 +0300

    Create MoneyLoader.cs

---
+++ MoneyLoader.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FOOPLab1
+{
+    class MoneyLoader
+    {
+        public void LoadNotes(ATM atm, string fileName)
+        {            
+            try
+            {
+                string[] strings = System.IO.File.ReadAllLines(fileName);
+                foreach (string note in strings)
+                {
+                    string[] str = note.Split(' ');
+                    atm.totalCash += Convert.ToInt32(str[0]) * Convert.ToInt32(str[1]);
+                    atm.notes.Add(Convert.ToInt32(str[0]), Convert.ToInt32(str[1]));
+                }
+            }
+            catch { }
+        }
+    }
+}
